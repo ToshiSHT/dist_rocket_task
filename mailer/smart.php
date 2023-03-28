d345e81863b3284179c385541d7695b926a5daa2
ToshiSht <Shetinin_anton@bk.ru>
2023-03-28 13:35:12 +0300

    setting mail

--- mailer/smart.php
+++ mailer/smart.php
@@ -5,18 +5,18 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
+$mail = new PHPMailer(true);
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'ssl://smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'shetinin_anton@bk.ru';                 // Наш логин
 $mail->Password = 'cmMnrcr8d5mq3gVQNfVs';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('shetinin_anton@bk.ru', 'Test task Rocket Business');   // От кого письмо 
 $mail->addAddress('shetinin_anton@bk.ru');     // Add a recipient
